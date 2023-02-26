6f5aff301f055b101ffa6115719f84ca99e7ba58
MoonMoohyeon <100409435+MoonMoohyeon@users.noreply.github.com>
2023-02-26 23:50:13 +0900

    [Gold V] Title: 아우으 우아으이야!!, Time: 44 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int X[100001] = {0, };
+int Y[100001] = {0, };
+
+int main(void)
+{
+	int N;
+	scanf("%d", &N);
+	int max = 0;
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d %d", &X[i], &Y[i]);
+		if (X[i] > max) max = X[i];
+		if (Y[i] > max) max = Y[i];
+	}
+
+	sort(X, X + N);
+	sort(Y, Y + N);
+
+	int sum = max - X[0];
+	for (int i = 0; i < N; i++)
+	{
+		if (Y[i] < X[i + 1])
+		{
+			sum -= X[i + 1] - Y[i];
+		}
+	}
+
+	printf("%d", sum);
+	
+	return 0;
+}
+
