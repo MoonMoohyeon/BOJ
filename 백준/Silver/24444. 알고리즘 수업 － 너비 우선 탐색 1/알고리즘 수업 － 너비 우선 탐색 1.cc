4783ef235cc4009750dac9ab97bdbc8c91f6af6d
MoonMoohyeon <100409435+MoonMoohyeon@users.noreply.github.com>
2023-07-03 19:09:42 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 116 ms, Memory: 9824 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<queue>
+#include<string.h>
+#include<algorithm>
+using namespace std;
+
+vector<int> graph[100001];
+int mark[100001] = { 0, };
+int res[100001] = { 0, };
+queue<int> q;
+int cnt = 0;
+
+void bfs(int r) {
+	q.push(r);
+	mark[r] = 1;
+	res[r] = ++cnt;
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+
+		for (int i = 0; i < graph[cur].size(); i++) {
+			int next = graph[cur][i];
+			if (!mark[next]) {
+				res[next] = ++cnt;
+				q.push(next);
+				mark[next] = 1;
+			}
+		}
+	}
+}
+int main() {
+	int n, m, r;
+	int a, b;
+	scanf("%d %d %d", &n, &m, &r);
+	for (int i = 1; i <= m; i++) {
+		scanf("%d %d", &a, &b);
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
+	bfs(r);
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", res[i]);
+	}
+
+}
