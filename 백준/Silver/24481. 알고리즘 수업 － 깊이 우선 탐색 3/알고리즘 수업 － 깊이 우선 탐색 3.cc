544590b87b498f1c87fa271100929aac2f404128
MoonMoohyeon <100409435+MoonMoohyeon@users.noreply.github.com>
2023-07-14 16:15:08 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 128 ms, Memory: 12748 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<queue>
+#include<string.h>
+#include<algorithm>
+using namespace std;
+
+vector<int> graph[100001];
+int mark[100001] = { 0, };
+int res[100001] = { 0, };
+queue<int> q;
+int cnt = 0;
+
+void dfs(int prev, int next) {
+	if (mark[next] == 1) return;
+	mark[next] = 1;
+	res[next] = res[prev] + 1;
+	for (int i = 0; i < graph[next].size(); i++)
+	{
+		dfs(next, graph[next][i]);
+	}
+}
+int main() {
+	int n, m, r;
+	int a, b;
+	memset(res, -1, sizeof(res));
+	scanf("%d %d %d", &n, &m, &r);
+	for (int i = 1; i <= m; i++) {
+		scanf("%d %d", &a, &b);
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
+	dfs(0, r);
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", res[i]);
+	}
+
+}
