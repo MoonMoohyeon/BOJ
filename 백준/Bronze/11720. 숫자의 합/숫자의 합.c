9f222a5ddc0445b8a6da500278594db1a04dccaf
MoonMoohyeon <100409435+MoonMoohyeon@users.noreply.github.com>
2023-08-05 16:40:33 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int sum = 0;
+    scanf("%d", &n);
+    char arr[n];
+    scanf("%s", &arr);
+    for(int i = 0; i < n; i++){
+        sum += arr[i] - '0';
+    }
+    printf("%d", sum);
+}
