9ab5545f3a1445d9ad483757ae9bc48fa9d5e8c4
MoonMoohyeon <100409435+MoonMoohyeon@users.noreply.github.com>
2023-08-11 01:36:33 +0900

    [Bronze I] Title: 소인수분해, Time: 32 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int N;
+
+	scanf("%d", &N);
+
+	for (int i = 2; i <= N; i++)
+	{
+		while (N % i == 0)
+		{
+			printf("%d\n", i);
+			N /= i;
+		}
+	}
+
+    return 0;
+}
