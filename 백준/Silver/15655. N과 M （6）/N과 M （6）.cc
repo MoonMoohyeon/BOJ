10e6c810f5028a648c5189cb2a588b517d029ace
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-08-03 11:45:52 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int N, M;
+int numbers[9] = { 0, };
+int visited[9] = { 0, };
+vector<int> selected;
+
+void NM(int cnt) {
+    if (cnt == M) {
+        for (auto i = selected.begin(); i != selected.end(); i++) {
+            cout << *i << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    int start = 0;
+    if (!selected.empty()) {
+        start = cnt;
+    }
+
+    for (int i = start; i < N; i++) {
+        if (!visited[i]) {
+            if (selected.empty() || selected.back() < numbers[i]) {
+                selected.push_back(numbers[i]);
+                visited[i] = 1;
+                NM(cnt + 1);
+                selected.pop_back();
+                visited[i] = 0;
+            }
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> numbers[i];
+    }
+    sort(numbers, numbers + N);
+    NM(0);
+
+    return 0;
+}
