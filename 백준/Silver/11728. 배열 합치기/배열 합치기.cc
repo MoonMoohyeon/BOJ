b7c489048a15ebd9ed2d4890a99e3d6837ba9e33
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-08-12 14:01:16 +0900

    [Silver V] Title: 배열 합치기, Time: 620 ms, Memory: 16736 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+int A[1000001];
+int B[1000001];
+int sum[2000003];
+
+int main(void)
+{
+	int N, M;
+
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &A[i]);
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		scanf("%d", &B[i]);
+	}
+
+	int ap = 0;
+	int bp = 0;
+	for (int i = 0; i < N + M; i++)
+	{
+		if (ap >= N)
+		{
+			sum[i] = B[bp++];
+		}
+		else if (bp >= M)
+		{
+			sum[i] = A[ap++];
+		}
+		else if (A[ap] >= B[bp])
+		{
+			sum[i] = B[bp++];
+		}
+		else
+		{
+			sum[i] = A[ap++];
+		}
+	}
+
+	for (int i = 0; i < N + M; i++)
+	{
+		printf("%d ", sum[i]);
+	}
+	
+	return 0;
+}
+
