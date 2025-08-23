e5876f7fb967a92fce5c1f10870affcac980bfb1
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-08-24 03:02:38 +0900

    [Gold II] Title: 계보 복원가 호석, Time: 104 ms, Memory: 5152 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+unordered_map<string, int> name_to_idx;
+unordered_map<int, string> idx_to_name;
+queue<int> q;
+vector<int> graph[1001];
+int indegree[1001];
+vector<int> root;
+vector<int> children[1001];
+int N, M;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N;
+    vector<string> names(N);
+    for (int i = 0; i < N; i++) {
+        cin >> names[i];
+    }
+    sort(names.begin(), names.end());
+
+    for (int i = 0; i < N; i++) {
+        name_to_idx[names[i]] = i;
+        idx_to_name[i] = names[i];
+    }
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        string parent, child;
+        cin >> child >> parent;
+        int pidx = name_to_idx[parent];
+        int cidx = name_to_idx[child];
+
+        graph[pidx].push_back(cidx);
+        indegree[cidx]++;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (indegree[i] == 0) {
+            q.push(i);
+            root.push_back(i);
+        }
+    }
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        for (auto i = graph[cur].begin(); i != graph[cur].end(); i++) {
+            if (--indegree[*i] == 0) {
+                q.push(*i);
+                children[cur].push_back(*i);
+            }
+        }
+    }
+
+    cout << root.size() << "\n";
+    vector<string> s;
+    for (auto i = root.begin(); i != root.end(); i++) {
+        cout << idx_to_name[*i] << " ";
+    }
+    cout << "\n";
+    
+    for (int i = 0; i < N; i++) {
+        cout << idx_to_name[i] << " " << children[i].size() << " ";
+        for (int j = 0; j < children[i].size(); j++) {
+            cout << idx_to_name[children[i][j]] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
