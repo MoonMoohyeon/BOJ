164254807a84da82c1f7f4be63bf749c32c75ac5
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-09-08 16:31:10 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int board[1001];
+int dp[1001];
+
+int main() {
+
+	int answer = 0;
+	int N; cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> board[i];
+	}
+
+
+	for (int i = 0; i < N; i++) {
+
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (board[j] < board[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		answer = max(dp[i], answer);
+	}
+	cout << answer << "\n";
+	return 0;
+}
