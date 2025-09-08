32a0d232798278e2eac328bd00cd75d95a1516e6
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-09-08 15:30:35 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -1,38 +1,29 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
-#include <stack>
-#include <limits.h>
-#include <string.h>
+#include <vector>
+
 using namespace std;
 
-int n, k;
+vector<int> v;
+int dp[10001];
 
-int DP[10001] = {0, };
-int values[101] = {0, }; 
-
-/*
-    1차원 테이블 갱신 = 
-    (n원짜리 동전을 사용하지 않고) v-n원을 만드는 경우의 수 + v원을 만드는 경우의 수
-*/
-
-int main(void)
+int main()
 {
-    scanf("%d %d", &n, &k);
-    for(int i=1; i<=n; i++)
-    {
-            scanf("%d", &values[i]);
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N, K; cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        int input; cin >> input;
+        v.push_back(input);
     }
 
-    DP[0] = 1;
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=values[i]; j<=k; j++)
-        {
-            DP[j] = DP[j] + DP[j - values[i]];
+    dp[0] = 1;
+    for (int i = 0; i< v.size(); i++) {
+        for (int j = v[i]; j <= K; j++) {
+            dp[j] += dp[j - v[i]];
         }
     }
+    cout << dp[K] << "\n";
 
-    printf("%d\n", DP[k]);
     return 0;
 }
